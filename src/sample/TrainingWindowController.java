8cc90b4af7e2046adcc62b6119dbe7588d7c7b8c
kolya19951 <kolya.simotyuk@gmail.com>
2017-04-10 22:36:06 +0300

    Interface v.3 Get arrays

--- src/sample/TrainingWindowController.java
+++ src/sample/TrainingWindowController.java
@@ -1,14 +1,28 @@
 package sample;
 
+import classes.Parser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import nn.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import static nn.algebra.Alg.rand;
 
 public class TrainingWindowController {
 
@@ -24,7 +38,19 @@ public class TrainingWindowController {
     @FXML
     private TextField depthField;
 
+    @FXML
+    private Button chooseXButton;
+
+    @FXML
+    private Button chooseYButton;
+
+    @FXML
+    private Button loadTrainButton;
+
     private ObservableList<TrainingView> trainingProperties = FXCollections.observableArrayList();
+
+    private byte[][] imgs;
+    private byte[] labels;;
 
 
     @FXML
@@ -49,5 +75,44 @@ public class TrainingWindowController {
 
         });
 
+        chooseYButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open X File");
+                try {
+                    labels = Parser.parseY(new RandomAccessFile(new File(fileChooser.showOpenDialog(new Stage()).toURI()) ,"rw"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+//                Image img = new Image(imgFile.toURI().toString());
+              //  imageViewer.setImage(img);
+            }
+        });
+
+        chooseXButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open X File");
+                try {
+                    imgs = Parser.parseX(new RandomAccessFile(new File(fileChooser.showOpenDialog(new Stage()).toURI()) ,"rw"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+//                Image img = new Image(imgFile.toURI().toString());
+                //  imageViewer.setImage(img);
+            }
+        });
+
+        loadTrainButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+            }
+        });
     }
+
 }
